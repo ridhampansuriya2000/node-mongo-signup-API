49c5e3f1fecd58731ee9ac470cfdc5434658d4f7
ridhampansuriya2000 <ridhampansuriya2000@gmail.com>
2023-07-19 00:43:54 +0530

    change vercel configuration

--- _helpers/swagger.js
+++ _helpers/swagger.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
+// const swaggerDocument = YAML.load('./swagger.yaml');
+const swaggerDocument = require('../swagger.yaml');
 
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
